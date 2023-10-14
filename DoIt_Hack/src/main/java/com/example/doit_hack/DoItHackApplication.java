b85281cd38e5363e724181f14d7c5bc24a98539a
Bhavya Rajput <98729874+Bhavooa@users.noreply.github.com>
2023-10-14 15:38:15 -0700

    Files
    files

---
+++ DoIt_Hack/src/main/java/com/example/doit_hack/DoItHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.doit_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItHackApplication.class, args);
+    }
+
+}
